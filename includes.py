5ab0cac6e7724962373af082fc9f9ecf14eb633c
David Brown <dbrown92700@gmail.com>
2021-11-20 02:21:13 -0500

    Builds OVA files for each of the sets of parameters in parameters.csv

--- includes.py
+++ includes.py
@@ -1,1 +1,2 @@
-basedir = '/Users/davibrow/Downloads/Cat8Kv/contents'
+ovadir = '/Users/davibrow/Downloads/Cat8Kv/test/contents'
+outdir = '/Users/davibrow/Downloads/Cat8Kv/test'
--- main.py
+++ main.py
@@ -3,32 +3,70 @@
 import os, hashlib, tarfile
 from includes import *
 
-# Build a OVA manifest file.  Calculates SHA1 hash for all files in directory 'basedir'
-# and writes a file name .mf that matches the ISO filename into basedir
-def writemanifest(basedir):
+# Build a OVA manifest file.  Calculates SHA1 hash for all files in directory 'ovadir'
+# and writes a file name .mf that matches the ISO filename into ovadir
+def writemanifest(ovadir):
     manifest = ''
-    files = os.listdir(basedir)
+    files = os.listdir(f'{ovadir}')
     for file in files:
         if file[0] != '.':
-            with open(f'{basedir}/{file}', 'rb') as filedata:
+            with open(f'{ovadir}/{file}', 'rb') as filedata:
                 hash = hashlib.sha1(filedata.read()).hexdigest()
                 manifest += f'SHA1({file})= {hash}\n'
                 if '.iso' in file:
                     filename = file[0:len(file)-4]
-    manfile = open(f'{basedir}/{filename}.mf','w')
+    manfile = open(f'{ovadir}/{filename}.mf', 'w')
     manfile.write(manifest)
-    return(filename)
 
-def tarova(basedir, filename):
+# Create OVA file with filename using files in outdir
+def tarova(ovadir, outdir, filename):
 
-    outfile = tarfile.open(f'{basedir}/{filename}.ova', 'w')
-    files = os.listdir(basedir)
+    outfile = tarfile.open(f'{outdir}/{filename}.ova', 'w')
+    files = os.listdir(ovadir)
     for file in files:
         if file[0] != '.':
-            outfile.add(f'{basedir}/{file}', arcname=f'{file}')
+            outfile.add(f'{ovadir}/{file}', arcname=f'{file}')
     outfile.close()
+
+def writeovf(ovadir, outdir, values):
+
+    files = os.listdir(ovadir)
+    for file in files:
+        if '.iso' in file:
+            ovffile = file[0:len(file)-4]
+            break
+    ovf = open(f'{ovadir}/{ovffile}.ovf','w')
+    format = open(f'{outdir}/format.ovf')
+    for line in format:
+        outline = line
+        if 'Property ovf:key=' in line:
+            keybegin = line.find('"')+1
+            keyend = line.find('"', keybegin)
+            key = line[keybegin:keyend]
+            try:
+                keyvalue = values[key]
+            except:
+                keyvalue = ''
+            print(line)
+            if keyvalue != '':
+                keybegin = line.find('ovf:value="')
+                outline = line[0:keybegin] + 'ovf:value="' + keyvalue + '">\n'
+        ovf.write(outline)
+    ovf.close()
+    format.close()
 
 if __name__ == '__main__':
 
-    filename = writemanifest(basedir)
-    tarova(basedir, filename)
+    parameters = open(f'{outdir}/parameters.csv', 'r', encoding='utf-8-sig')
+    header = parameters.readline().strip('\n').split(',')
+    for line in parameters:
+        data = line.strip('\n').split(',')
+        values = {}
+        index = 0
+        for parameter in header:
+            values[parameter] = data[index]
+            index += 1
+        print(values)
+        writeovf(ovadir, outdir, values)
+        writemanifest(ovadir)
+        tarova(ovadir, outdir, values['hostname'])
